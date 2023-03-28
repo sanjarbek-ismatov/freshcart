d1c11f87e22be35f7392304797cea4ed0e24f2b2
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-03-28 17:08:53 +0500

    feat: menu added

--- frontend/app/components/Menu/index.tsx
+++ frontend/app/components/Menu/index.tsx
@@ -1,8 +1,35 @@
-import './Menu.css'
+import Link from "next/link";
+import "./Menu.css";
 function Menu() {
   return (
     <>
-      <p>Hello, Menu</p>
+      <div className="flex items-center">
+        <div className="relative group ">
+          <button className="py-3 px-4 bg-green-500 rounded-md text-white">
+            Barchasi
+          </button>
+          <ul className="absolute transition-all translate-y-2 ease-in duration-300 group-hover:opacity-100 group-hover:translate-y-[0px] opacity-0 border text-slate-600 border-slate-300 p-3 w-36 rounded-md">
+            <li className="py-1">
+              <Link href="/">Maishiy texnika</Link>
+            </li>
+            <li className="py-1">
+              <Link href="/">Sport anjomlari</Link>
+            </li>
+            <li className="py-1">
+              <Link href="/">Oziq-ovqat</Link>
+            </li>
+          </ul>
+        </div>
+        <Link href="/" className="mx-3 text-slate-600 font-medium">
+          Uy
+        </Link>
+        <Link href="/" className="mx-3 text-slate-600 font-medium">
+          Maxsulotlar
+        </Link>
+        <Link href="/" className="mx-3 text-slate-600 font-medium">
+          Do`konlar
+        </Link>
+      </div>
     </>
   );
 }
