9dcd54a31578c05a684a5eaf7c65c75e2de21b1b
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-03-28 16:39:43 +0500

    removed bootstrap && tailwind initialized && navbar pending

--- frontend/app/components/Navbar/index.tsx
+++ frontend/app/components/Navbar/index.tsx
@@ -1,49 +1,42 @@
 import Image from "next/image";
-import React from "react";
-import Logo from "public/images/logo/freshcart-logo.svg";
-const Index = () => {
+import LogoImage from "public/images/logo/freshcart-logo.svg";
+import "./Navbar.css";
+function Navbar() {
   return (
-    <div className="row align-items-center my-3">
-      <div className="col">
-        <Image src={Logo} alt="Brand logo" />
-      </div>
-      <div className="col-xl-6 col-lg-6">
-        <div className="input-group align-items-center">
+    <>
+      <div className="flex justify-center py-5 items-center">
+        <div className="w-56 mr-12">
+          <Image src={LogoImage} alt="Logo image" />
+        </div>
+        <div className="w-full">
           <input
-            className="form-control"
-            placeholder="Xohlagan mahsulotingizni qidiring"
-            required
+            type="text"
+            className="w-8/12 outline-none px-3 py-2 border-slate-300 border rounded-sm"
+            placeholder="Maxsulotlarni qidiring"
           />
-          <div className="input-group-append position-relative d-flex align-items-center">
-            <i
-              style={{ right: "10px" }}
-              className="fa-solid fa-magnifying-glass position-absolute "
-            ></i>
-          </div>
+          <button className="mx-4 py-3 px-5 text-slate-500 rounded-md bg-slate-200">
+            Hudud
+          </button>
+        </div>
+        <div className="w-32">
+          <span className="relative">
+            <i className="fa-regular fa-heart text-xl"></i>
+            <span className="text-sm px-1 text-white rounded-full absolute top-[-10px] right-[-10px] bg-green-500">
+              1
+            </span>
+          </span>
+          <span className="relative mx-3">
+            <i className="fa-regular fa-user text-xl"></i>
+          </span>
+          <span className="relative">
+            <i className="fa-regular fa-bookmark text-xl"></i>
+            <span className="text-sm px-1 text-white rounded-full absolute top-[-10px] right-[-10px] bg-green-500">
+              1
+            </span>
+          </span>
         </div>
       </div>
-      <div className="col-xl-3 col-lg-2">
-        <button className="btn btn-outline-secondary">Joylashuv</button>
-      </div>
-      <div className="col">
-        <span className="position-relative">
-          <i className="fa-regular fa-heart"></i>
-          <span className="position-absolute text-bg-success top-0 start-100 translate-middle badge rounded-pill">
-            1
-          </span>
-        </span>
-        <span className="position-relative mx-3">
-          <i className="fa-regular fa-user"></i>
-        </span>
-        <span className="position-relative">
-          <i className="fa-regular fa-bookmark"></i>
-          <span className="position-absolute text-bg-success top-0 start-100 translate-middle badge rounded-pill">
-            1
-          </span>
-        </span>
-      </div>
-    </div>
+    </>
   );
-};
-
-export default Index;
+}
+export default Navbar;
--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,4 @@
 import "./globals.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Create Next App",
