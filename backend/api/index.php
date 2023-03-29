2a0fba6b284a2256a338eaf72feb77625806b138
AbdullajonovBoburjon <86303222+foxdevuz@users.noreply.github.com>
2023-03-29 22:14:58 +0500

    Some functions added
    getAllProducts
    getProductInfo
    getProfleInfo

--- backend/api/index.php
+++ backend/api/index.php
@@ -9,54 +9,55 @@
     $url = explode("/", $_SERVER['QUERY_STRING']);
     $method = $url[1];
 
-    if($method == 'addProduct'){
-        if(isset($_REQUEST['productName'])
-            && isset($_REQUEST['token'])
-            && isset($_REQUEST['cost'])
-            && isset($_REQUEST['productOwner'])
-            && isset($_FILES['img1'])
-            && isset($_FILES['img2'])
-            && isset($_FILES['img3'])
-            && isset($_REQUEST['description'])
-            && isset($_REQUEST['category'])
-            && isset($_REQUEST['weight'])){
-            //check owner in db
-            $token = $_REQUEST['token'];
-            $slt = $db->selectWhere('users',[
-                'temporary_token'=>$token,
-                'cn'=>'='
-            ]);
-            if(mysqli_num_rows($slt)>0){
-                // user found start writing datas to db
-                $name = $_REQUEST['productName'];
-                $owner = $_REQUEST['productOwner'];
-                $category = $_REQUEST['category'];
-                $about = $_REQUEST['description'];
-                $weight = $_REQUEST['weight'];
-                $cost= $_REQUEST['cost'];
-                $img1 = $db->escapeString($_FILES['img1']['name']);
-                $img2 = $db->escapeString($_FILES['img2']['name']);
-                $img3 = $db->escapeString($_FILES['img3']['name']);
-                // download media
-                $typeMedia1 = pathinfo($img1, PATHINFO_EXTENSION);
-                $typeMedia2 = pathinfo($img2, PATHINFO_EXTENSION);
-                $typeMedia3 = pathinfo($img3, PATHINFO_EXTENSION);
+    if($method){
+        if($method == 'addProduct'){
+            if(isset($_REQUEST['productName'])
+                && isset($_REQUEST['token'])
+                && isset($_REQUEST['cost'])
+                && isset($_REQUEST['productOwner'])
+                && isset($_FILES['img1'])
+                && isset($_FILES['img2'])
+                && isset($_FILES['img3'])
+                && isset($_REQUEST['description'])
+                && isset($_REQUEST['category'])
+                && isset($_REQUEST['weight'])){
+                //check owner in db
+                $token = $_REQUEST['token'];
+                $slt = $db->selectWhere('users',[
+                    'temporary_token'=>$token,
+                    'cn'=>'='
+                ]);
+                if(mysqli_num_rows($slt)>0){
+                    // user found start writing datas to db
+                    $name = $_REQUEST['productName'];
+                    $owner = $_REQUEST['productOwner'];
+                    $category = $_REQUEST['category'];
+                    $about = $_REQUEST['description'];
+                    $weight = $_REQUEST['weight'];
+                    $cost= $_REQUEST['cost'];
+                    $img1 = $db->escapeString($_FILES['img1']['name']);
+                    $img2 = $db->escapeString($_FILES['img2']['name']);
+                    $img3 = $db->escapeString($_FILES['img3']['name']);
+                    // download media
+                    $typeMedia1 = pathinfo($img1, PATHINFO_EXTENSION);
+                    $typeMedia2 = pathinfo($img2, PATHINFO_EXTENSION);
+                    $typeMedia3 = pathinfo($img3, PATHINFO_EXTENSION);
 
-                // check file type
-                if(in_array($typeMedia1, $allowed_media)
-                    && in_array($typeMedia2, $allowed_media)
-                    && in_array($typeMedia3, $allowed_media)){
+                    // check file type
+                    if(in_array($typeMedia1, $allowed_media)
+                        && in_array($typeMedia2, $allowed_media)
+                        && in_array($typeMedia3, $allowed_media)){
                         // ckeck file in `images` folder
                         if(file_exists('../assets/images/'.$img1)
                             && file_exists('../assets/images/'.$img2)
                             && file_exists('../assets/images/'.$img3)){
-                                // the file is excist in images folder so that give random name for it
-                                $img1 = bin2hex(random_bytes(8)) . $img1;
-                                $img2 = bin2hex(random_bytes(8)) . $img2;
-                                $img3 = bin2hex(random_bytes(8)) . $img3;
+                            // the file is excist in images folder so that give random name for it
+                            $img1 = bin2hex(random_bytes(8)) . $img1;
+                            $img2 = bin2hex(random_bytes(8)) . $img2;
+                            $img3 = bin2hex(random_bytes(8)) . $img3;
                         }
-                            // write datas to db and download media
-                            $ins = $db->insertInto('products',[
+                        // write datas to db and download media
+                        $ins = $db->insertInto('products',[
                             'product_name'=>$name,
                             'product_owner'=>$owner,
                             'about_product'=>$about,
@@ -85,18 +86,18 @@
                         $data['ok'] = 403;
                     }
 
-            } else {
-                $data['message'] = 'User not found, Adding product is forbidden';
-                $data['code'] = 403;
-            }
+                } else {
+                    $data['message'] = 'User not found, Adding product is forbidden';
+                    $data['code'] = 403;
+                }
 
-        } else {
-            $data['message'] = "There are no all required datas";
-            $data['code'] = 404;
+            } else {
+                $data['message'] = "There are no all required datas";
+                $data['code'] = 404;
+            }
         }
-    } 
-    if($method == 'signup'){
-        if(isset($_REQUEST['name']) && isset($_REQUEST['username']) && isset($_REQUEST['phone']) && isset($_REQUEST['email']) && isset($_REQUEST['password'])){
+        else if($method == 'signup'){
+            if(isset($_REQUEST['name']) && isset($_REQUEST['username']) && isset($_REQUEST['phone']) && isset($_REQUEST['email']) && isset($_REQUEST['password'])){
                 $name = $db->escapeString($_REQUEST['name']);
                 $username = $db->escapeString($_REQUEST['username']);
                 $phone = $db->escapeString($_REQUEST['phone']);
@@ -118,7 +119,6 @@
                         'cn'=>'='
                     ]
                 );
-//                print_r($slt->num_rows);
                 if(!$slt->num_rows>0){
                     $pass = md5($pass);
                     $ins = $db->insertInto('users',[
@@ -146,38 +146,97 @@
                 $data['code'] = 404;
                 $data['message'] = "There are not all reqired datas";
             }
-    } 
-    if($method == 'login') {
-        if(isset($_REQUEST['email']) && isset($_REQUEST['password'])){
-            $email = $db->escapeString($_REQUEST['email']);
-            $password = $db->escapeString($_REQUEST['password']);
-            $password = md5($password);
+        }
+        else if($method == 'login') {
+            if(isset($_REQUEST['email']) && isset($_REQUEST['password'])){
+                $email = $db->escapeString($_REQUEST['email']);
+                $password = $db->escapeString($_REQUEST['password']);
+                $password = md5($password);
 
-            // check db for user 
-            $check = $db->selectWhere('users',
-                [
-                    'password'=>$password,
-                    'cn'=>'=',
-                ],
-                [
-                    'email'=>$email,
-                    'cn'=>'='
-                ]    
-            );
-            if($check-num_rows>0){
-                foreach ($check as $key => $value) {
-                    $data['result'][] = $value;
+                // check db for user
+                $check = $db->selectWhere('users',
+                    [
+                        'password'=>$password,
+                        'cn'=>'=',
+                    ],
+                    [
+                        'email'=>$email,
+                        'cn'=>'='
+                    ]
+                );
+                if($check->num_rows>0){
+                    foreach ($check as $key => $value) {
+                        $data['result'][] = $value;
+                    }
+                    $data['ok'] = true;
+                    $data['code'] = 200;
+                    $data['message'] = "User found";
+                } else {
+                    $data['code'] = 404;
+                    $data['message'] = 'Email or password is not suitable';
                 }
-                $data['ok'] = true;
-                $data['code'] = 200;
-                $data['message'] = "User found";
             } else {
                 $data['code'] = 404;
-                $data['message'] = 'Email or password is not suitable';
+                $data['message'] = "Email and password are required";
             }
-        } else {    
-            $data['code'] = 404;
-            $data['message'] = "Email and password are required";
+        }
+        else if($method == 'getAllProducts'){
+            $all = $db->selectAll('products');
+            if($all->num_rows > 0){
+                $data['ok'] = true;
+                $data['code'] = 200;
+                $data['message'] = "Product found";
+                foreach ($all as $key => $val){
+                    $data['result'] = $val;
+                }
+            } else {
+                $data['code'] = 404;
+                $data['message'] = "Product not found";
+            }
+        }
+        else if($method == 'getInfoProduct'){
+            if(isset($_REQUEST['id'])){
+                $id = $db->escapeString($_REQUEST['id']);
+                $info = $db->selectWhere('products',
+                    [
+                        'id'=>$id,
+                        'cn'=>'='
+                    ]
+                );
+                if($info->num_rows > 0){
+                    $data['ok'] = true;
+                    $data['code'] = 200;
+                    $data['message'] = "Product info";
+                    foreach ($info as $key => $val){
+                        $data['result'][] = $val;
+                    }
+                } else {
+                    $data['code'] = 404;
+                    $data['message'] = "Product not found";
+                }
+            }
+        }
+        else if($method == 'getProfileInfo'){
+            if(isset($_REQUEST['token'])){
+                $token = $db->escapeString($_REQUEST['token']);
+                $getinfo = $db->selectWhere('users',
+                    [
+                        'temporary_token'=>$token,
+                        'cn'=>'='
+                    ]
+                );
+                if($getinfo->num_rows > 0){
+                    $data['ok'] = true;
+                    $data['code'] = 200;
+                    $data['message'] = "User found";
+                    foreach ($getinfo as $key => $val){
+                        $data['result'][] = $val;
+                    }
+                } else {
+                    $data['code'] = 404;
+                    $data['message'] = "User not found | Please redirect to login page!!!";
+                }
+            }
         }
     }
 
