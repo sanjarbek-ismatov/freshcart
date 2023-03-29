9afaaba4a3b6af38fe5326468ea7c38eebad7698
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-03-29 21:30:20 +0500

    feat: popular products && categories

---
+++ frontend/app/components/Catergories/index.tsx
@@ -0,0 +1,45 @@
+"use client";
+import "./Catergories.css";
+import data from "@/data/categories.json";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper";
+import Image from "next/image";
+import Link from "next/link";
+function Catergories() {
+  return (
+    <>
+      <Swiper
+        spaceBetween={30}
+        slidesPerView={6}
+        modules={[Autoplay]}
+        centeredSlides={true}
+        loop
+        autoplay={{
+          delay: 1500,
+          disableOnInteraction: false,
+        }}
+        className="my-5"
+      >
+        {data.map((e, i) => (
+          <SwiperSlide
+            key={i}
+            className="w-72 border border-gray-300 p-5 flex justify-center flex-col items-center"
+          >
+            <Link href={e.path}>
+              <Image
+                src={e.image}
+                width={100}
+                height={0}
+                alt="Category image"
+                loader={() => e.image}
+                unoptimized
+              />
+              <p className="my-3 font-medium text-slate-700">{e.name}</p>
+            </Link>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
+}
+export default Catergories;
