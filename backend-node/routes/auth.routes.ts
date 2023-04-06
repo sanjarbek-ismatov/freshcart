c859e73ad1c363e1747d1adfdb68946ee646df01
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-04-06 21:32:53 +0500

    fix

--- backend-node/routes/auth.routes.ts
+++ backend-node/routes/auth.routes.ts
@@ -1,10 +1,5 @@
 import express from "express";
 import authController from "../controllers/auth.controller";
-import { upload } from "../models/gridfs.model";
 const authRouter = express.Router();
-authRouter.post(
-  "/signup",
-  upload.single("image"),
-  authController.signUpController
-);
+authRouter.post("/signup", authController.signUpController);
 export { authRouter };
