df6595749463b1d9e993a9e276d7525b09240dd7
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-04-06 12:12:49 +0000

    feat: init validator

---
+++ backend-node/helpers/validator.ts
@@ -0,0 +1,31 @@
+import Joi from "joi";
+import JoiPasswordComplexity from "joi-password-complexity";
+export const registerValidator = (body: {
+    name: string;
+    username: string;
+    email: string;
+    phone: string;
+    password: string
+}) => {
+    const validator = Joi.object({
+        name: Joi.string().required().min(3),
+        username: Joi.string().required().min(3),
+        email: Joi.string().required().min(3),
+        phone: Joi.string().required().min(10),
+        password: Joi.string().required().min(8)
+    })
+    const passwordComplexity = JoiPasswordComplexity({
+        max: 256,
+        min: 8,
+        symbol: 1,
+        lowerCase: 1,
+        upperCase: 1,
+        numeric: 1
+    }, 'password')
+    const {error: errorBody} = validator.validate(body)
+    const {error: errorPassword} = passwordComplexity.validate(body.password)
+    if(errorBody)
+        return errorBody
+    if(errorPassword) return errorPassword
+    else return null
+}
--- backend-node/server.ts
+++ backend-node/server.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import routes from "./routes/routes";
 import "./helpers/logger";
+import {config} from 'dotenv'
+config()
+import { dbConnect } from "./helpers/dbconnector";
 const app = express();
+dbConnect()
 routes(app);
 app.listen(process.env.PORT || 4000, () => console.log("Server is running..."));
