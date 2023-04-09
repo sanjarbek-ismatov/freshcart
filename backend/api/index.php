faadf4797822b5f38a1a8ddeb9dc3fbdde587d37
AbdullajonovBoburjon <86303222+foxdevuz@users.noreply.github.com>
2023-04-09 22:29:34 +0500

    Getting Data type is changed

--- backend/api/index.php
+++ backend/api/index.php
@@ -1,5 +1,4 @@
 <?php
-    header('Access-Control-Allow-Origin: http://localhost:3000');
     $data = ['ok'=>false,'code'=>null,'message'=>null,'result'=>[]];
     $allowed_media = ['jpg','png','jpeg','svg'];
     // call db config
