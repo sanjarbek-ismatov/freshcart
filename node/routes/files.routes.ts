d3797290e6cd02d298a5d3895385b3817881f94c
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-04-28 20:41:18 +0500

    streaming files

--- node/routes/files.routes.ts
+++ node/routes/files.routes.ts
@@ -1,6 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
+import Grid from "gridfs-stream";
 const fileRoute = express.Router()
-fileRoute.get('/image/:filename', async (req, res) => {
+var gfs: Grid.Grid, gfb;
+const connection = mongoose.connection
+connection.once('open', () => {
+    gfs = Grid(connection.db, mongoose.mongo)
+    gfs.collection('uploads')
+    gfb = new mongoose.mongo.GridFSBucket(connection.db, {bucketName: 'uploads'})
+})
 
+fileRoute.get('/image/:filename', async (req, res) => {
+     gfs.files.find().toArray((err, file) => {
+        if(err) console.log(err)
+        console.log(file)
+    })
+     gfs.files.findOne({filename: req.params.filename}, (err, file) => {
+        if(err) console.log(err)
+        console.log(file)
+    })
+    res.status(200).send('ok')
 })
 export default fileRoute
