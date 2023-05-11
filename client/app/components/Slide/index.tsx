9208e705f188887b743b22c220985edfd150c0e1
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-05-11 17:22:45 +0500

    some fix

--- client/app/components/Slide/index.tsx
+++ client/app/components/Slide/index.tsx
@@ -8,10 +8,11 @@ function Slide() {
     previewImage?.addEventListener("mousemove", (e) => {
       const x = (e.offsetX / 600) * 100;
       const y = (e.offsetY / 600) * 100;
-      previewImage.style.backgroundSize = "1000px";
+      previewImage.style.backgroundSize = "1200px";
       previewImage.style.backgroundPosition = `${x}% ${y}%`;
     });
     previewImage?.addEventListener("mouseout", (e) => {
+      previewImage.style.backgroundSize = "cover";
       previewImage.style.backgroundPosition = "center";
     });
   }, []);
@@ -22,7 +23,7 @@ function Slide() {
           backgroundImage:
             'url("https://freshcart.codescandy.com/assets/images/products/product-single-img-1.jpg")',
         }}
-        className="w-[600px] h-[600px] bg-center"
+        className="w-[600px] h-[600px] bg-center hover:cursor-zoom-in"
         id="previewImage"
       ></div>
       <div></div>
