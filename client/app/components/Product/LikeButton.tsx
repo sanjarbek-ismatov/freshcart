1515dccdcdcbb84c9fc51e306bce424ee0842cf0
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-05-22 16:32:53 +0500

    fix bug

--- client/app/components/Product/LikeButton.tsx
+++ client/app/components/Product/LikeButton.tsx
@@ -1,28 +1,40 @@
 "use client";
-
-import { useAddToCartMutation } from "@/store/api/ecommerce";
-import { useCallback, useMemo } from "react";
 import { useUserContext } from "@/app/context";
+import { useCallback, useMemo, useState } from "react";
+import { useAddToCartMutation } from "@/store/api/ecommerce";
 
 function LikeButton({ id }: { id: string }) {
   const data = useUserContext();
+
+  const [addToLiked, { isSuccess }] = useAddToCartMutation();
+
   const isLiked = useMemo(
-    () => data?.liked.find((e) => e._id.toString() === id.toString()),
+    () =>
+      Boolean(
+        data?.liked.find((e) => e._id.toString() === id.toString())
+      ).valueOf(),
     [data?.liked, id]
   );
-  const [addToLiked] = useAddToCartMutation();
+
+  const [liked, setLiked] = useState(() => isLiked);
+
   const submitLiked = useCallback(() => {
-    isLiked &&
+    if (!liked) {
       addToLiked({
         type: "like",
         id,
       });
-  }, [addToLiked, id, isLiked]);
+      setLiked(true);
+    } else {
+      console.log(false);
+    }
+  }, [addToLiked, id, liked]);
+
   return (
     <button onClick={submitLiked}>
       <i
-        className={` ${
-          isLiked ? "text-red-600 fa-solid" : "fa-regular"
+        className={`${
+          liked ? "text-red-600 fa-solid" : "fa-regular"
         } bg-white py-2 px-3 rounded-lg cursor-pointer hover:bg-green-500 hover:text-white transition-colors ease-in duration-100 text-slate-900 mx-1 text-2xl fa-heart`}
       ></i>
     </button>
