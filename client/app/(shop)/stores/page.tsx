b923b976a22f03a9e557ca8308f88f8d02c41b09
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-05-28 08:39:50 +0500

    shops rendered

--- client/app/(shop)/stores/page.tsx
+++ client/app/(shop)/stores/page.tsx
@@ -1,14 +1,20 @@
-"use client";
 import { Footer } from "@/app/components";
 import { Header, StoresGrid } from "./components";
+import { getSSRData } from "@/app/utils/getData";
+import { VendorType } from "@/types";
 
-function Stores() {
+async function Stores() {
+  const vendors = await getSSRData<VendorType[]>(
+    "http://localhost:4000/api/vendor/all"
+  );
+
   return (
     <>
       <Header />
-      <StoresGrid />
+      <StoresGrid vendors={vendors} />
       <Footer />
     </>
   );
 }
+
 export default Stores;
