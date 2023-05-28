efbebb52fd7e278bfac31bd03ac148fabb27ad9f
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-05-28 09:08:27 +0500

    single shop rendering

--- client/app/(shop)/stores/[slug]/page.tsx
+++ client/app/(shop)/stores/[slug]/page.tsx
@@ -1,5 +1,18 @@
-function Store({ params }: { params: { slug: string } }) {
-  return <p>Hello {params.slug}</p>;
+import { getSSRData } from "@/app/utils/getData";
+import { VendorType } from "@/types";
+import { Products } from "@/app/(shop)/products/components";
+import { Header } from "@/app/(shop)/stores/components";
+
+async function Store({ params }: { params: { slug: string } }) {
+  const vendor = await getSSRData<VendorType>(
+    `http://localhost:4000/api/vendor/${params.slug}`
+  );
+  return (
+    <>
+      <Header title={vendor.name} image={vendor.image} />{" "}
+      <Products products={vendor.products} />
+    </>
+  );
 }
 
 export default Store;
