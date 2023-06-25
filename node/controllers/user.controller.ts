9eb5d41c5af60a1cc9e5cee7f5aaee881f7bf890
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-06-25 20:07:27 +0500

    server: vendor populating fixed

--- node/controllers/user.controller.ts
+++ node/controllers/user.controller.ts
@@ -38,8 +38,12 @@ async function loginController(req: NodeRequest, res: Response) {
 }
 
 async function getInfo(req: NodeRequest, res: Response) {
-  const user = await req.user?.populate("cart.id cart.id.vendor liked");
-  console.log(user?.cart);
+  const user = await req.user?.populate({
+    path: "cart.id liked",
+    populate: {
+      path: "vendor",
+    },
+  });
   res.status(200).send(user);
 }
 
@@ -47,7 +51,7 @@ async function addToCart(req: NodeRequest, res: Response) {
   const user = req.user;
   if (req.body.type === "cart") {
     const index = user?.cart.findIndex((e) => e.id.toString() === req.body.id);
-    console.log(index);
+
     index !== -1
       ? user?.cart.splice(index || -1, 1)
       : user?.cart.unshift(req.body);
