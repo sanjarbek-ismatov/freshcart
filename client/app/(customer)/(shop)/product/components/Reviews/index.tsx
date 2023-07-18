19a400a3e5982e7e2a97c1439680f95d20dd2c27
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-07-18 15:33:39 +0500

    client: minimal design

--- client/app/(customer)/(shop)/product/components/Reviews/index.tsx
+++ client/app/(customer)/(shop)/product/components/Reviews/index.tsx
@@ -1,30 +1,54 @@
 import "./Reviews.css";
 import { ReviewType } from "@types";
-import { ProfileImage } from "@components";
+import { ProfileImage, Star, Typography } from "@components";
+import Image from "next/image";
 
 function Reviews({ reviews }: { reviews: ReviewType[] }) {
   return (
-    <div>
-      <h4>Izohlar</h4>
-      {reviews.map((e, i) => (
-        <div key={i}>
-          {/*<Image*/}
-          {/*  height={50}*/}
-          {/*  width={50}*/}
-          {/*  src={`http://localhost:4000/api/files/image/${e.clientId.image}`}*/}
-          {/*  alt="Profil rasmi"*/}
-          {/*  unoptimized*/}
-          {/*/>*/}
-          <ProfileImage
-            image={`http://localhost:4000/api/files/image/${e.clientId.image}`}
-            size={50}
-          />
-          {/*</div>*/}
-          <div>
-            <h4>{e.clientId.name}</h4>
+    <div className="flex">
+      <div className="w-[300px]"></div>
+      <div>
+        <Typography text="Izohlar" />
+        {reviews.map((e, i) => (
+          <div className="flex" key={i}>
+            {/*<Image*/}
+            {/*  height={50}*/}
+            {/*  width={50}*/}
+            {/*  src={`http://localhost:4000/api/files/image/${e.clientId.image}`}*/}
+            {/*  alt="Profil rasmi"*/}
+            {/*  unoptimized*/}
+            {/*/>*/}
+            <ProfileImage
+              image={`http://localhost:4000/api/files/image/${e.clientId.image}`}
+              size={50}
+            />
+            {/*</div>*/}
+            <div>
+              <div>
+                <h4>{e.clientId.name}</h4>
+                <p>{new Date(e.date).toDateString()}</p>
+              </div>
+              <div>
+                <Star rating={e.star} />
+                <p>{e.body}</p>
+                <div className="flex gap-3">
+                  {e.images.map((e, i) => (
+                    <Image
+                      key={i}
+                      width={70}
+                      height={70}
+                      className="w-[70px] h-[70px] object-cover"
+                      src={`http://localhost:4000/api/files/image/${e}`}
+                      alt="Rasmlar"
+                      unoptimized
+                    />
+                  ))}
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
