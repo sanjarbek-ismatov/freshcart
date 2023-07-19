ae23eaa1b91de08843dfffac251e3ce4b545961b
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-07-19 17:43:00 +0500

    client: stats of reviews

--- client/app/(customer)/(shop)/product/components/Reviews/index.tsx
+++ client/app/(customer)/(shop)/product/components/Reviews/index.tsx
@@ -1,7 +1,33 @@
+import "./Reviews.css";
 import { ReviewType } from "@types";
 import { Star, Typography } from "@components";
 import { ReviewCard } from "@/app/(customer)/(shop)/product/components";
 import { useMemo } from "react";
+
+function StarLine({
+  ratingLevel,
+  percent,
+}: {
+  ratingLevel: number;
+  percent: number;
+}) {
+  return (
+    <div className="flex w-full items-center">
+      <div className="flex items-center">
+        <span className="text-slate-500">{ratingLevel}</span>
+        <i className="fa-solid text-sm text-yellow-500 fa-star p-1"></i>
+      </div>
+      <div className="bg-slate-300 w-full relative rounded-sm h-1.5">
+        <div
+          style={{
+            width: percent + "%",
+          }}
+          className="bg-yellow-500 absolute top-0 left-0 rounded-sm h-1.5"
+        ></div>
+      </div>
+    </div>
+  );
+}
 
 function Reviews({ reviews }: { reviews: ReviewType[] }) {
   const generalStars = useMemo(() => {
@@ -18,12 +44,20 @@ function Reviews({ reviews }: { reviews: ReviewType[] }) {
   }, [reviews]);
   return (
     <div className="flex">
-      <div className="w-[300px]">
+      <div className="w-[300px] p-3 mr-4">
         <Typography text="Statistika" />
-        <Star rating={generalStars} />
-        {/*<i className="fa-solid text-sm text-yellow-500 fa-star"></i>*/}
+        <span>
+          <Star rating={generalStars} /> {reviews.length}ta izoh
+        </span>
+        <div className="my-4 leading-8">
+          <StarLine ratingLevel={5} percent={70} />
+          <StarLine ratingLevel={4} percent={50} />
+          <StarLine ratingLevel={3} percent={20} />
+          <StarLine ratingLevel={2} percent={100} />
+          <StarLine ratingLevel={1} percent={30} />
+        </div>
       </div>
-      <div>
+      <div className="flex-1">
         <Typography text={`Izohlar (${reviews.length})`} />
         {reviews.map((e, i) => (
           <ReviewCard key={i} review={e} />
--- client/app/components/Star/index.tsx
+++ client/app/components/Star/index.tsx
@@ -2,12 +2,12 @@ import "./Star.css";
 
 function Star({ rating }: { rating: number }) {
   return (
-    <div>
+    <div className="inline">
       {Array.from(new Array(5), (v, k) => k + 1).map((e, i) => (
         <i
           key={i}
           className={`fa-solid text-sm ${
-            e <= rating ? "text-yellow-500" : "text-slate-400"
+            e <= rating ? "text-yellow-500" : "text-slate-300"
           } fa-star`}
         ></i>
       ))}
