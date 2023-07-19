7e9c26ae1e28ffd158534f69cc869b7240a84e43
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-07-19 14:26:45 +0500

    client: review and star updated

--- client/app/components/Star/index.tsx
+++ client/app/components/Star/index.tsx
@@ -3,8 +3,13 @@ import "./Star.css";
 function Star({ rating }: { rating: number }) {
   return (
     <div>
-      {Array.from(new Array(rating || 0), (v, k) => k + 1).map((e, i) => (
-        <i key={i} className="fa-solid text-sm text-yellow-500 fa-star"></i>
+      {Array.from(new Array(5), (v, k) => k + 1).map((e, i) => (
+        <i
+          key={i}
+          className={`fa-solid text-sm ${
+            e <= rating ? "text-yellow-500" : "text-slate-400"
+          } fa-star`}
+        ></i>
       ))}
     </div>
   );
