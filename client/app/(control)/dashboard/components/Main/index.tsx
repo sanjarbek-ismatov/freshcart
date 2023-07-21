b4bfa54b3f344cd8a78b1a891d3636d6cd77fbf0
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-07-21 15:08:27 +0500

    client: vendor details was updated && dashboard

--- client/app/(control)/dashboard/components/Main/index.tsx
+++ client/app/(control)/dashboard/components/Main/index.tsx
@@ -1,21 +1,42 @@
-import "./Main.css";
 import { Banner, Card, Chart, Order } from "@components/dashboard";
+import { VendorWithOrders } from "@types";
 
-function Main() {
+function Main({ data }: { data?: VendorWithOrders }) {
+  const lastOrders = data?.orders.filter(
+    (e) => e.status === "pending" || e.status === "processing",
+  );
+  const lastOrdersTotalPrice = lastOrders?.reduce(
+    (accu, curr) => accu + curr.totalPrice,
+    0,
+  );
+  const ordersinlastTwoWeeks = data?.orders.filter((e) => {
+    const date = new Date();
+    date.setDate(date.getDate() - 7);
+    const orderDate = new Date(e.date).getTime();
+    return orderDate > date.getTime();
+  });
+  const users = new Set(data?.orders.map((e) => e.clientId._id));
+
   return (
     <>
       <Banner />
       <div className="flex">
         <Card
           title="Ishlangan summa"
-          main="9000$"
+          main={`${data?.vendor.annualIncome ?? 0}$`}
           submain="Bir yillik natija"
         />
-        <Card title="Buyurtmalar" main="500$" submain="12ta yangi buyurtma!" />
+        <Card
+          title="Buyurtmalar"
+          main={`${lastOrdersTotalPrice ?? 0}$`}
+          submain={`${lastOrders?.length ?? 0}ta yangi buyurtma!`}
+        />
         <Card
           title="Xaridorlar"
-          main="40ta"
-          submain="Oxirgi 2 kun ichida 10ta"
+          main={`${users.size ?? 0}ta`}
+          submain={`Oxirgi 1 hafta ichida ${
+            ordersinlastTwoWeeks?.length ?? 0
+          }ta`}
         />
       </div>
 
--- client/app/(control)/dashboard/page.tsx
+++ client/app/(control)/dashboard/page.tsx
@@ -2,13 +2,14 @@
 import { Main } from "@components/dashboard";
 import { useAuth } from "@/app/hooks/useAuth";
 import { useRouter } from "next/navigation";
+import { useGetControllerInfoQuery } from "@/store/api/ecommerce";
 
 function VendorDashboardPage() {
   const auth = useAuth("vendor");
-
+  const { data } = useGetControllerInfoQuery();
   const router = useRouter();
   // if (!auth) router.replace("/dashboard/auth/login");
-  return <Main />;
+  return <Main data={data} />;
 }
 
 export default VendorDashboardPage;
