b08e748e7b5f9501bee95a72d8e61ba90c96032f
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-08-02 13:56:49 +0500

    server: admin login event

--- node/controllers/admin.controller.ts
+++ node/controllers/admin.controller.ts
@@ -2,6 +2,7 @@ import Admin from "../models/admin.model";
 import { NodeRequest } from "../types";
 import { Response } from "express";
 import { passwordChecker, passwordGenerator } from "../helpers/passwordmanager";
+import { tokenGenerator } from "../helpers/tokengenerator";
 
 async function createAdmin(req: NodeRequest, res: Response) {
   const { key, password, login } = req.query;
@@ -20,8 +21,15 @@ async function createAdmin(req: NodeRequest, res: Response) {
 
 async function loginAdmin(req: NodeRequest, res: Response) {
   const admin = await Admin.findOne({ login: req.body.login });
-  if (!admin) return res.status(401).send({ code: 401, message: "Xato parol" });
+  if (!admin)
+    return res.status(404).send({ code: 404, message: "Mavjud emas admin" });
   const checkPass = await passwordChecker(req.body.password, admin.password);
+  if (!checkPass)
+    return res.status(401).send({ code: 401, message: "Xato parol" });
+  const token = tokenGenerator("", "login", admin.login);
+  res
+    .setHeader("x-admin-token", token)
+    .send({ code: 200, message: "Bajarildi" });
 }
 
-export default { createAdmin };
+export default { createAdmin, loginAdmin };
