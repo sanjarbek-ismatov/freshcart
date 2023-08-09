b84f8a7d90609b8e4db9b92170037516071d3088
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-08-09 14:38:06 +0500

    seo control

--- node/controllers/general.controller.ts
+++ node/controllers/general.controller.ts
@@ -7,9 +7,17 @@ class GeneralController {
     const info = await Site.findOne();
     res.status(200).send(info);
   }
+
   async updateInfo(req: NodeRequest, res: Response) {
-    const info = await Site.findOneAndUpdate({}, { ...req.body });
+    const info = await Site.findOne({});
+    if (!info) return;
+    if (req.file) {
+      info.image = req.file.filename;
+    }
+    Object.assign(info, req.body);
+    await info.save();
     res.status(200).send(info);
   }
 }
+
 export default GeneralController;
