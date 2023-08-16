9fb90bafe0fe585f74b61b8e3f2ce402551131dc
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-08-16 19:36:11 +0500

    client: main page design

--- client/app/[lng]/components/SidePanel/index.tsx
+++ client/app/[lng]/components/SidePanel/index.tsx
@@ -1,16 +1,24 @@
+"use client";
 import Button from "../Button";
 import SearchInput from "../SearchInput";
 import "./SidePanel.css";
 import { getTranslation } from "@internalization";
+import { useState } from "react";
 
 function SidePanel() {
+  const [show, setShow] = useState(false);
   const t = getTranslation("uz");
   return (
     <>
-      <div className="fixed md:hidden block h-screen w-96 bg-white top-0 left-0 z-30 p-4">
+      <div
+        style={{
+          left: show ? "0" : "-100%",
+        }}
+        className={`fixed  md:hidden block h-screen w-96 bg-white top-0 left-0 z-30 p-4`}
+      >
         <div className="flex justify-between items-center my-3">
           <span>{t.menu}</span>
-          <i className="fa-solid fa-xmark"></i>
+          <i onClick={() => setShow(!show)} className="fa-solid fa-xmark"></i>
         </div>
         <div>
           <SearchInput full placeholder={t["enter-product-name"]} />
