f0fa60929f4f495dbf7c05d90a90180c00f8f106
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-09-01 13:30:08 +0500

    project: panel navigation

--- client/app/[lng]/(control)/dashboard/components/PanelNavigation/index.tsx
+++ client/app/[lng]/(control)/dashboard/components/PanelNavigation/index.tsx
@@ -9,59 +9,76 @@ function PanelNavigation() {
   const lastPath = pathname.split("/").at(-1);
 
   return (
-    <div className="w-[400px] leading-10 p-5">
+    <div className="group w-[100px] hover:w-[300px] leading-10 p-5 transition-all overflow-hidden">
       <Link href="/">
         <Image src={LogoImage} alt="logo" />
       </Link>
       <Link
-        className={`py-2 px-3 text-gray-500  block my-4 ${
+        className={`py-2 px-3 text-gray-500 w-[300px] block my-4 ${
           lastPath === "dashboard" && "bg-green-200 text-green-600"
         }`}
         href="/dashboard"
       >
-        <i className="fa-solid fa-house fa-bounce"></i> Bosh sahifa
+        <i className="fa-solid fa-house fa-bounce"></i>{" "}
+        <span className="opacity-0 break-normal group-hover:opacity-100">
+          Bosh sahifa
+        </span>
       </Link>
-      <p className="text-sm text-gray-500">Do`kon boshqaruvi</p>
+      <p className="opacity-0 text-gray-500 group-hover:opacity-100 w-[300px]">
+        Do`kon boshqaruvi
+      </p>
       <ul>
-        <li className="my-3">
+        <li className="my-3 w-[300px]">
           <Link
             className={`p-3 block text-gray-500 ${
               lastPath === "products" && "bg-green-200 text-green-600"
             }`}
             href="/dashboard/products"
           >
-            <i className="fa-solid fa-cart-shopping mx-1"></i>Maxsulotlar
+            <i className="fa-solid fa-cart-shopping mx-1"></i>
+            <span className="opacity-0 break-normal group-hover:opacity-100">
+              Maxsulotlar
+            </span>
           </Link>
         </li>
-        <li className="my-3">
+        <li className="my-3 w-[300px]">
           <Link
             className={`p-3 block text-gray-500 ${
               lastPath === "orders" && "bg-green-200 text-green-600"
             }`}
             href="/dashboard/orders"
           >
-            <i className="fa-solid fa-bag-shopping mx-1"></i>Xaridlar
+            <i className="fa-solid fa-bag-shopping mx-1"></i>
+            <span className="opacity-0 break-normal group-hover:opacity-100">
+              Xaridlar
+            </span>
           </Link>
         </li>
 
-        <li className="my-3">
+        <li className="my-3 w-[300px]">
           <Link
             className={`p-3 block text-gray-500 ${
               lastPath === "reviews" && "bg-green-200 text-green-600"
             }`}
             href="/dashboard/reviews"
           >
-            <i className="fa-regular fa-star mx-1"></i>Izohlar
+            <i className="fa-regular fa-star mx-1"></i>
+            <span className="opacity-0 break-normal group-hover:opacity-100">
+              Izohlar
+            </span>
           </Link>
         </li>
-        <li className="my-3">
+        <li className="my-3 w-[300px]">
           <Link
             className={`p-3 block text-gray-500 ${
               lastPath === "discounts" && "bg-green-200 text-green-600"
             }`}
             href="/dashboard/discounts"
           >
-            <i className="fa-solid fa-tag mx-1"></i> Chegirmalar
+            <i className="fa-solid fa-tag mx-1"></i>
+            <span className="opacity-0 break-normal group-hover:opacity-100">
+              Chegirmalar
+            </span>
           </Link>
         </li>
       </ul>
