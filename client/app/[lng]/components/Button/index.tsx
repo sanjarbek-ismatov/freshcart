70576abb4725c3178d4d5162f86c6dce5c39ba8b
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-09-03 15:38:25 +0500

    client: button component has been updated

--- client/app/[lng]/components/Button/index.tsx
+++ client/app/[lng]/components/Button/index.tsx
@@ -4,17 +4,25 @@ import "./Button.css";
 function Button({
   children,
   full,
+  color,
+  bgColor,
   ...rest
 }: {
   children: ReactNode;
   full?: boolean;
+  color?: string;
+  bgColor?: string;
 } & ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
     <button
       {...rest}
       className={`${
         full && "w-full"
-      } px-3 text-sm disabled:bg-gray-300 rounded-lg my-2 py-2 bg-green-500 border-4 active:border-green-300 text-white  inline`}
+      } px-3 text-sm disabled:bg-gray-300 rounded-lg my-2 py-2 ${
+        bgColor ? bgColor : "bg-green-500"
+      } border-4 active:border-green-300 ${
+        color ? color : "text-white"
+      } inline`}
     >
       {children}
     </button>
